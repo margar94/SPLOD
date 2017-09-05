42aac1c0997a2825d3e214f1ff2be3834b1360a9
margar94 <margar1994@gmail.com>
2017-09-05 16:47:19 +0200

    fix filler

--- boxFiller.js
+++ boxFiller.js
@@ -23,23 +23,57 @@ BoxFiller.prototype.updateConcepts = function(entityUrl, entityLabel, callback){
 BoxFiller.prototype.updatePredicates = function(selectedConcept, predUrl, predLabel, predicateDirection, callback){
 	
 	var limit = false;
-	executor.getSelectedEntityPredicates(predUrl, limit, function(data){
-		callback(data);
+	
+	var directData;
+	var reverseData;
+
+	var d1 = $.Deferred(executor.getSelectedEntityDirectPredicates(predUrl, limit, function(data){
+		directData = data;
+		d1.resolve();
+	}));
+	var d2 = $.Deferred(executor.getSelectedEntityReversePredicates(predUrl, limit, function(data){
+		reverseData = data;
+		d2.resolve();
+	}));
+
+	$.when(d1, d2).done(function(){
+
+		var resultObj = {
+			directArray: [],
+			reverseArray: [],
+			attributesArray: directData
+		};
+
+		$.each(directData, function(index){
+
+			directData[index].verb = "that has";
+		});
+		resultObj.directArray = directData;
+
+		$.each(reverseData, function(index){
+			reverseData[index].verb = "that is";
+		});
+		resultObj.reverseArray = reverseData;
+
+		callback(resultObj);
 	});
+
 
 	if(!selectedConcept)
 		queryVerbalizator.selectedPredicate(predUrl, predLabel, predicateDirection);
 }
 
 BoxFiller.prototype.retrievePredicates = function(callback) {
+	var limit = 20;
+
 	var directData;
 	var reverseData;
 
-	var d1 = $.Deferred(executor.getAllDirectPredicates(20, function(data){
+	var d1 = $.Deferred(executor.getAllDirectPredicates(limit, function(data){
 		directData = data;
 		d1.resolve();
 	}));
-	var d2 = $.Deferred(executor.getAllReversePredicates(20, function(data){
+	var d2 = $.Deferred(executor.getAllReversePredicates(limit, function(data){
 		reverseData = data;
 		d2.resolve();
 	}));
