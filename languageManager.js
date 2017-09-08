642f75c312be8abb2fdfd8b6550a48e1d948559f
margar94 <margar1994@gmail.com>
2017-09-08 10:14:07 +0200

    LanguageManager updated

--- languageManager.js
+++ languageManager.js
@@ -9,3 +9,67 @@ LanguageManager.prototype.getArticle = function(noun){
 	else article = "a";
 	return article;
 }
+
+LanguageManager.prototype.verbalizeConcept = function(conceptLabel){
+	verbalization = {
+		standard: [],
+		modified: [],
+		truncated: [],
+		first: [],
+		current: "standard"};
+
+	verbalization.standard.push(getArticle(conceptLabel) + ' ');
+	verbalization.standard.push(conceptLabel + ' ');
+
+	//verbalization.modiefied.push();
+
+	//verbalization.truncated.push();
+
+	verbalization.first.push('every ');
+	verbalization.first.push(conceptLabel + ' ');
+
+}
+
+LanguageManager.prototype.verbalizePredicate = function(predicateLabel, predicateDirection){
+	verbalization = {
+		standard: [],
+		modified: [],
+		truncated: [],
+		first: [],
+		current: "standard"};
+
+
+	if(predicateDirection == 'direct'){
+
+		verbalization.standard.push('that has ' + getArticle(predicateLabel) + ' ');
+		verbalization.standard.push(predicateLabel + ' ');
+
+		verbalization.modiefied.push('whose ');
+		verbalization.modiefied.push(predicateLabel + ' ');
+
+		verbalization.truncated.push('has ' + getArticle(predicateLabel) + ' ');
+		verbalization.truncated.push(predicateLabel + ' ');
+
+		verbalization.first.push('everything that has ');
+		verbalization.first.push(conceptLabel + ' ');
+
+	}else if(predicateDirection == 'reverse'){
+
+		verbalization.standard.push('that is the ');
+		verbalization.standard.push(predicateLabel + ' ');
+		verbalization.standard.push('of ');
+
+		verbalization.modiefied.push('of ');
+		verbalization.modiefied.push(predicateLabel + ' ');
+
+		verbalization.truncated.push('is the ');
+		verbalization.truncated.push(predicateLabel + ' ');
+		verbalization.truncated.push('of ');
+
+		verbalization.first.push('the ');
+		verbalization.first.push(conceptLabel + ' ');
+		verbalization.first.push('of ');
+	}
+	
+
+}
