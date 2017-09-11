d8344e90d1111018612c29eecb737e19a1ea8d15
mariaangelapellegrino <mariaangelapellegrino94@gmail.com>
2017-09-11 12:47:39 +0200

    BoxViewer : update boxFiller calls

--- boxViewer.js
+++ boxViewer.js
@@ -1,139 +1,134 @@
 
-	var boxFiller;
-	var queryVerbalizer;
-	var languageManager;
+var boxFiller;
+var languageManager;
+
+function initBoxViewer(){
+	boxFiller = new BoxFiller();
+	languageManager = new LanguageManager();
+}
+
+function fillConcepts(){
+	boxFiller.retrieveConcepts(function (concepts){
+		renderConcepts(concepts);
+	});
+}
+
+function fillPredicates(){
+	boxFiller.retrievePredicates(function (predicates){
+		renderPredicates(predicates);
+	});
+}
+
+function renderConcepts(concepts){
+	var conceptsList = $("#conceptsList");
+	conceptsList.empty();
+
+	$.each(concepts, function(index){
+		element = concepts[index];
+		var li = $("<li/>")
+			.attr('title', element.url)
+			.attr('meta-url', element.url)
+			.attr('meta-label', element.label)
+			.text(element.label)
+			.appendTo(conceptsList)
+			.on('click', function(){
+				boxFiller.updateConceptsFromConcept($(this).attr('meta-url'), $(this).attr('meta-label'), renderConcepts);
+				boxFiller.updatePredicatesFromConcept($(this).attr('meta-url'), $(this).attr('meta-label'), false, renderPredicates);
+			});
+	});
+}
+
+function renderPredicates(predicates){
+	var predicatesList = $("#predicatesList");
+	predicatesList.empty();
+
+	var directArray = predicates.directArray;
+	var reverseArray = predicates.reverseArray;
+	var attributesArray = predicates.attributesArray;
 	
-	function initBoxViewer(){
-		boxFiller = new BoxFiller();
-		queryVerbalizator = new QueryVerbalizator();
-		languageManager = new LanguageManager();
-	}
-
-	function fillConcepts(){
-		boxFiller.retrieveConcepts(function (concepts){
-			renderConcepts(concepts);
-		});
-	}
+	renderAttributes(attributesArray);
+	renderDirectPredicates(directArray);
+	renderReversePredicates(reverseArray);
 	
-	function fillPredicates(){
-		boxFiller.retrievePredicates(function (predicates){
-			renderPredicates(predicates);
-		});
-	}
+}
 
-	function renderConcepts(concepts){
-		var conceptsList = $("#conceptsList");
-		conceptsList.empty();
+function renderDirectPredicates(directArray){
 
-		$.each(concepts, function(index){
-			element = concepts[index];
-			var li = $("<li/>")
-				.attr('title', element.url)
-				.attr('meta-url', element.url)
-				.attr('meta-label', element.label)
-				.text(element.label)
-				.appendTo(conceptsList)
-				.on('click', function(){
-					boxFiller.updateConcepts($(this).attr('meta-url'), $(this).attr('meta-label'), renderConcepts);
-					boxFiller.updatePredicatesFromConcept($(this).attr('meta-url'), $(this).attr('meta-label'), false, renderPredicates);
-				});
-		});
-	}
+	var li = $("<li/>")
+		.css('backgroundColor', 'yellow')
+		.text('Direct predicates')
+		.appendTo(predicatesList);
 
-	function renderPredicates(predicates){
-		var predicatesList = $("#predicatesList");
-		predicatesList.empty();
-
-		var directArray = predicates.directArray;
-		var reverseArray = predicates.reverseArray;
-		var attributesArray = predicates.attributesArray;
+	$.each(directArray, function(index){
+		element = directArray[index];
+		article = languageManager.getArticle(element.label);
 		
-		renderAttributes(attributesArray);
-		renderDirectPredicates(directArray);
-		renderReversePredicates(reverseArray);
-		
-	}
+		var li = $("<li/>")
+			.attr('title', element.url)
+			.attr('meta-url', element.url)
+			.attr('meta-label', element.label)
+			.attr('meta-verb', element.verb)
+			.attr('meta-article', element.article)
+			.attr('meta-predicateDirection', 'direct') 
+			.text(element.verb +" "+ article +" "+ element.label)
+			.appendTo(predicatesList)
+			.on('click', function(){
+				boxFiller.updateConceptsFromDirectPredicate($(this).attr('meta-url'), $(this).attr('meta-label'), renderConcepts);
+				boxFiller.updatePredicatesFromPredicate($(this).attr('meta-url'), $(this).attr('meta-label'), $(this).attr('meta-predicateDirection'), renderPredicates);
+			});
+	});
+}
 
-	function renderDirectPredicates(directArray){
+function renderReversePredicates(reverseArray){
+
+	var li = $("<li/>")
+		.css('backgroundColor', 'yellow')
+		.text('Reverse predicates')
+		.appendTo(predicatesList);
+
+	$.each(reverseArray, function(index){
+		element = reverseArray[index];
+		article = languageManager.getArticle(element.label);
 
 		var li = $("<li/>")
-			.css('backgroundColor', 'yellow')
-			.text('Direct predicates')
-			.appendTo(predicatesList);
+			.attr('title', element.url)
+			.attr('meta-url', element.url)
+			.attr('meta-label', element.label)
+			.attr('meta-verb', element.verb)
+			.attr('meta-article', element.article)
+			.attr('meta-predicateDirection', 'reverse')
+			.text(element.verb +" "+ article +" "+ element.label)
+			.appendTo(predicatesList)
+			.on('click', function(){
+				boxFiller.updateConceptsFromReversePredicate($(this).attr('meta-url'), $(this).attr('meta-label'), renderConcepts);
+				boxFiller.updatePredicatesFromPredicate($(this).attr('meta-url'), $(this).attr('meta-label'), $(this).attr('meta-predicateDirection'), renderPredicates);
+			});
+	});
+}
 
-		$.each(directArray, function(index){
-			element = directArray[index];
-			article = languageManager.getArticle(element.label);
-			
-			var li = $("<li/>")
-				.attr('title', element.url)
-				.attr('meta-url', element.url)
-				.attr('meta-label', element.label)
-				.attr('meta-verb', element.verb)
-				.attr('meta-article', element.article)
-				.attr('meta-predicateDirection', 'direct') 
-				.text(element.verb +" "+ article +" "+ element.label)
-				.appendTo(predicatesList)
-				.on('click', function(){
-					//disabilita i concetti
+function renderAttributes(attributesArray){
 
-					boxFiller.updatePredicatesFromPredicate($(this).attr('meta-url'), $(this).attr('meta-label'), $(this).attr('meta-predicateDirection'), renderPredicates);
-				});
-		});
-	}
+	var li = $("<li/>")
+		.css('backgroundColor', 'yellow')
+		.text('Attributes')
+		.appendTo(predicatesList);
 
-	function renderReversePredicates(reverseArray){
-
-		var li = $("<li/>")
-			.css('backgroundColor', 'yellow')
-			.text('Reverse predicates')
-			.appendTo(predicatesList);
-
-		$.each(reverseArray, function(index){
-			element = reverseArray[index];
-			article = languageManager.getArticle(element.label);
-
-			var li = $("<li/>")
-				.attr('title', element.url)
-				.attr('meta-url', element.url)
-				.attr('meta-label', element.label)
-				.attr('meta-verb', element.verb)
-				.attr('meta-article', element.article)
-				.attr('meta-predicateDirection', 'reverse')
-				.text(element.verb +" "+ article +" "+ element.label)
-				.appendTo(predicatesList)
-				.on('click', function(){
-					boxFiller.updateConceptsFromReversePredicate($(this).attr('meta-url'), $(this).attr('meta-label'), renderConcepts);
-					boxFiller.updatePredicatesFromPredicate($(this).attr('meta-url'), $(this).attr('meta-label'), $(this).attr('meta-predicateDirection'), renderPredicates);
-				});
-		});
-	}
-
-	function renderAttributes(attributesArray){
+	$.each(attributesArray, function(index){
+		element = attributesArray[index];
+		article = "";
+		article = languageManager.getArticle(element.label);
 
 		var li = $("<li/>")
-			.css('backgroundColor', 'yellow')
-			.text('Attributes')
-			.appendTo(predicatesList);
-
-		$.each(attributesArray, function(index){
-			element = attributesArray[index];
-			article = "";
-			article = languageManager.getArticle(element.label);
-
-			var li = $("<li/>")
-				.attr('title', element.url)
-				.attr('meta-url', element.url)
-				.attr('meta-label', element.label)
-				.text(element.label)
-				.appendTo(predicatesList)
-				.on('click', function(){
-					//disabilita i concetti
-					console.log($(this).attr('meta-url'));
-					boxFiller.selectedAttribute($(this).attr('meta-url'), $(this).attr('meta-label'));
-				});
-		});
-	}
+			.attr('title', element.url)
+			.attr('meta-url', element.url)
+			.attr('meta-label', element.label)
+			.text(element.label)
+			.appendTo(predicatesList)
+			.on('click', function(){
+				boxFiller.selectedAttribute($(this).attr('meta-url'), $(this).attr('meta-label'));
+			});
+	});
+}
 
 
 
