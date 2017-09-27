0dd54f587aae928118e68b347a497b2b26ef98fa
mariaangelapellegrino <mariaangelapellegrino94@gmail.com>
2017-09-27 12:04:22 +0200

    add utilPresentationLayer

---
+++ utilPresentationLayer.js
@@ -0,0 +1,35 @@
+function highlightElementsToRemove(){
+	var IDtoRemove = $('.highlighted').attr('meta-removereference');
+	if(IDtoRemove != undefined){
+		var spanToRemove = document.getElementById(IDtoRemove).parentNode.querySelectorAll('span');
+		document.getElementById(IDtoRemove).parentNode.className += ' highlightRemove';
+
+		$.each(spanToRemove, function(index){
+			spanToRemove[index].className += ' highlightRemove';
+		});
+	}
+}
+
+function removeHighlightElementsToRemove(){
+	var toRemoveHightlight = $('.highlightRemove');
+
+	$.each(toRemoveHightlight, function(index){
+		toRemoveHightlight[index].setAttribute('class', toRemoveHightlight[index].getAttribute('class').replace(' highlightRemove', ''));
+	});
+}
+
+function filter(inputName, boxName) {
+    var input, filter, box, li, text, i;
+    input = document.getElementById(inputName);
+    filter = input.value.toLowerCase();
+   	box = document.getElementById(boxName);
+    li = box.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        text = li[i].innerHTML;
+        if (text.toLowerCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
