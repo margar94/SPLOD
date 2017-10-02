33a16c893284b61e1787797e7531caf1fd136d4d
margar94 <margar1994@gmail.com>
2017-10-02 15:50:24 +0200

    render reusable result list

--- operatorViewer.js
+++ operatorViewer.js
@@ -16,6 +16,7 @@ function renderResult(select, labelSelect, results){
 function renderOperatorList(operators){
 	var operatorList = $('#operatorList');
 	operatorList.empty();
+	$('#operatorList').show();
 
 	$.each(operators, function(index){
 		var element = operators[index];
@@ -27,6 +28,32 @@ function renderOperatorList(operators){
 		.appendTo(operatorList)
 		.on('click', function(){
 			operatorManager.selectedOperator($(this).attr('meta-value'));
+			if(!operatorManager.isComplete()){
+				var reusableResults = operatorManager.getResultToCompleteOperator();
+				renderReusableResultList(reusableResults);
+			}
+		});
+	});
+
+}
+
+function renderReusableResultList(reusableResults){
+
+	var operatorList = $('#operatorList').hide();
+
+	var reusableResultList = $('#reusableResultList');
+	reusableResultList.empty();
+
+	$.each(reusableResults, function(index){
+		var element = reusableResults[index];
+
+		var li = $("<li/>")
+		.attr('class', 'collection-item')
+		.attr('meta-label', element.value)
+		.text(element.value)
+		.appendTo(reusableResultList)
+		.on('click', function(){
+			console.log("vmfdjbvjhfvb");
 		});
 	});
 
