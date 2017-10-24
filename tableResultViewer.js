fa64a147211649272f2638778b8c051aedf652b2
mariaangelapellegrino <mariaangelapellegrino94@gmail.com>
2017-10-24 15:30:45 +0200

    clean previewTableResult when focus is null and dropdown menu for table's fields

--- tableResultViewer.js
+++ tableResultViewer.js
@@ -6,10 +6,23 @@ function initTableResultViewer(){
 	languageManager = new LanguageManager();
 
 	$('#tableResultSpinner').hide();
+
+	$('.dropdown-button').dropdown({
+	      inDuration: 300,
+	      outDuration: 225,
+	      constrainWidth: false, // Does not change width of dropdown to that of the activator
+	      hover: true, // Activate on hover
+	      gutter: 0, // Spacing from edge
+	      belowOrigin: true, // Displays dropdown below the button
+	      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+	      stopPropagation: false, // Stops event propagation
+	    }
+	  );
 }
 
 function resetResultTable(){
 	$('#resultsTable').empty();
+	$('#previewTableResult').empty();
 	$('#tableResultSpinner').hide();
 }
 
